48334f61e1e0153d91da206fea6c7bf945b62114
Bohdan Hutnik <bohdanhutnik@gmail.com>
2024-05-03 23:41:31 +0200

    complette 4/5  3.5.2024

--- src/tictactoe/tictactoe.java
+++ src/tictactoe/tictactoe.java
@@ -6,36 +6,112 @@ public class tictactoe {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-
-        String game = input.nextLine();
-
-        String[][] g = {
-            {" ", " ", " "},
-            {" ", " ", " "},
-            {" ", " ", " "}
+        String[][] gameMatrix = {
+            {" "," ", " ", " ", " "},
+            {"|"," ", " ", " ", "|"},
+            {"|"," ", " ", " ", "|"},
+            {"|"," ", " ", " ", "|"}
         };
 
-        int numLenGame = 0;
-        for (int i = 0; i < g.length; i++) {
-            for (int j = 0; j < g[i].length; j++) {
-                g[i][j] = String.valueOf(game.charAt(numLenGame));
-                numLenGame++;
+        loadGame(gameMatrix);
 
+        printGame(gameMatrix);
+
+        MoveX(gameMatrix);
+
+        printGame(gameMatrix);
+
+        MoveX(gameMatrix);
+
+        printGame(gameMatrix);
+
+        Wins(gameMatrix);
+
+
+        input.close();
+        
+    }
+    
+
+    public static void loadGame (String[][] g) {
+        Scanner input = new Scanner(System.in);
+        String game = input.nextLine();
+
+        if (!game.isEmpty() || game.length() < 8) {
+            if (game.length() < 9) {
+                for (int i = game.length(); i < 9; i++) {
+                    game += "_";
+                }
+            }
+            int numLenGame = 0;
+            for (int i = 1; i < g.length; i++) {
+                for (int j = 1; j < g[i].length - 1; j++) {
+                    g[i][j] = String.valueOf(game.charAt(numLenGame));
+                    numLenGame++;
+
+                }
             }
         }
-
-        String g1 = g[0][0];
-        String g2 = g[0][1];
-        String g3 = g[0][2];
-        String g4 = g[1][0];
-        String g5 = g[1][1];
-        String g6 = g[1][2];
-        String g7 = g[2][0];
-        String g8 = g[2][1];
-        String g9 = g[2][2];
+    }
 
 
-        int count_X = 0;
+    public static void printGame(String[][] gameArray){
+
+        System.out.println("---------");
+        for (int i = 1; i < gameArray.length; i++) {
+            for (int j = 0; j < gameArray[i].length; j++) {
+                System.out.print(gameArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("---------");
+        
+    }
+
+
+    public static void MoveX( String[][] gameArray) {
+        Scanner input = new Scanner(System.in);
+        
+        boolean numInputFlag;
+        int moveY = 0;
+        int moveX = 0;
+        
+        do {
+            numInputFlag = true;
+            if (input.hasNextInt()) {
+
+                moveX = input.nextInt();
+                moveY = input.nextInt();
+
+                if (moveX > 3 || moveX < 0 || moveY > 3 || moveY < 0) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                } else if ("X".equals(gameArray[moveX][moveY]) || "O".equals(gameArray[moveX][moveY])) {
+                    System.out.println("This cell is occupied! Choose another one!");
+                }
+                
+
+            } else {
+                System.out.println("You should enter numbers!");
+                numInputFlag = false;
+                input.nextLine();
+            }
+
+        } while (moveX > 3 || moveX < 0 || moveY > 3 || moveY < 0 || "X".equals(gameArray[moveX][moveY]) || "O".equals(gameArray[moveX][moveY]) || !numInputFlag);
+        gameArray[moveX][moveY] = "X";
+    }
+
+
+    public static void Wins (String[][] g) {
+        String g1 = g[1][0];
+        String g2 = g[1][1];
+        String g3 = g[1][2];
+        String g4 = g[2][0];
+        String g5 = g[2][1];
+        String g6 = g[2][2];
+        String g7 = g[3][0];
+        String g8 = g[3][1];
+        String g9 = g[3][2];
+
         boolean xWin = false;
         if ("XXX".equals(g1 + g2 + g3) || "XXX".equals(g4 + g5 + g6) || "XXX".equals(g7 + g8 + g9) || "XXX".equals(g1 + g5 + g9)) {
             xWin = true;
@@ -43,7 +119,6 @@ public class tictactoe {
             xWin = true;
         }
 
-        int count_O = 0;
         boolean oWin = false;
         if ("OOO".equals(g1 + g2 + g3) || "OOO".equals(g4 + g5 + g6) || "OOO".equals(g7 + g8 + g9) || "OOO".equals(g1 + g5 + g9)) {
             oWin = true;
@@ -51,31 +126,21 @@ public class tictactoe {
             oWin = true;
         }
 
+        int count_O = 0;
+        int count_X = 0;
+        int Impossible_O_X = 0;
         boolean gameNotFinished = false;
-
-
-        System.out.println("---------");
-        for (int i = 0; i < g.length; i++) {
-            System.out.print("| ");
+        for (int i = 1; i < g.length; i++) {
             for (int j = 0; j < g[i].length; j++) {
-                if (g[i][j].equals("_")) {
-                    gameNotFinished = true;
-                    
-                }
-                if (g[i][j].equals("X")) {
-                    count_X++;
-                } else if (g[i][j].equals("O")) {
-                    count_O++;
-                }
-
-                System.out.print(g[i][j] + " ");
+                if (g[i][j].equals("_")) gameNotFinished = true;
+                if (g[i][j].equals("X")) count_X++;
+                if (g[i][j].equals("O")) count_O++;
             }
-            System.out.print("|\n");
         }
-        System.out.println("---------");
+        Impossible_O_X = Math.abs(count_X - count_O);
 
 
-        if ((((count_X - count_O) >= 2 || (count_O - count_X) >= 2)) && (gameNotFinished && !xWin && !oWin) || (xWin && oWin)) {
+        if ((Impossible_O_X >= 2) && (gameNotFinished && !xWin && !oWin) || (xWin && oWin)) {
             System.out.println("Impossible");
         } else if (gameNotFinished && !xWin && !oWin) {
             System.out.println("Game not finished");
@@ -86,8 +151,6 @@ public class tictactoe {
         } else if (oWin == true) {
             System.out.println("O wins");
         }
-
-        input.close();
     }
 }
 
